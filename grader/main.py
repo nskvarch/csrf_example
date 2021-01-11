5fd5dec23cea9394b979a0dddbdea2cc16d96606
Niels Skvarch <nielsk01@noa.nintendo.com>
2021-01-11 14:38:38 -0800

    lesson 8 activity

--- grader/main.py
+++ grader/main.py
@@ -1,23 +1,27 @@
 import os
-import base64
-
+import random
 from flask import Flask, request, session
 from model import Grade 
+
 
 app = Flask(__name__)
 app.secret_key = "dFbVyccvTuNpkUP2x6vcnZWN"
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if 'csrf_token' not in session:
+        session['csrf_token'] = str(random.randint(10000000, 99999999))
 
     if request.method == 'POST':
-        g = Grade(
-            student=request.form['student'],
-            assignment=request.form['assignment'],
-            grade=request.form['grade'],
-        )
-        #print("(" + request.form['grade'] + ")")
-        g.save()
+        if request.form.get('csrf_token', None) == session['csrf_token']:
+            g = Grade(
+                student=request.form['student'],
+                assignment=request.form['assignment'],
+                grade=request.form['grade'],
+            )
+            # print("(" + request.form['grade'] + ")")
+            g.save()
 
     body = """
 <html>
@@ -34,12 +38,14 @@ def home():
 
     <label for="grade">Grade</label>
     <input type="text" name="grade"><br>
+    
+    <input type="hidden" name="csrf_token" value="{}"> 
 
     <input type="submit" value="Submit">
 </form>
 
 <h2>Existing Grades</h2>
-"""
+""".format(session['csrf_token'])
     
     for g in Grade.select():
         body += """
@@ -54,4 +60,3 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6779))
     app.run(host='0.0.0.0', port=port)
-
